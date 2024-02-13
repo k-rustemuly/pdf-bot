18eac764f027449c7e581f81ee50f90d4f2de11e
Kuanysh <developerandroidkz@gmail.com>
2024-02-13 20:25:14 +0500

    upload command

---
+++ app/Services/Commands/User/UploadCommand.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Services\Commands\User;
+
+use Longman\TelegramBot\Commands\UserCommand;
+
+use Longman\TelegramBot\Entities\Keyboard;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Request;
+
+/**
+ * Start command
+ */
+class UploadCommand extends UserCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'upload';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Upload and save files';
+
+    /**
+     * @var string
+     */
+    protected $usage = '/upload';
+
+    /**
+     * @var string
+     */
+    protected $version = '0.2.0';
+
+    /**
+     * @var bool
+     */
+    protected $need_mysql = true;
+
+    /**
+     * Main command execution
+     *
+     * @return ServerResponse
+     * @throws TelegramException
+     */
+    public function execute(): ServerResponse
+    {
+        $message = $this->getMessage();
+        $chat    = $message->getChat();
+        $chat_id = $chat->getId();
+        $user_id = $message->getFrom()->getId();
+
+        // Make sure the Download path has been defined and exists
+        $download_path = $this->telegram->getDownloadPath();
+        if (!is_dir($download_path)) {
+            return $this->replyToChat('Download path has not been defined or does not exist.');
+        }
+
+        // Initialise the data array for the response
+        $data = ['chat_id' => $chat_id];
+
+        if ($chat->isGroupChat() || $chat->isSuperGroup()) {
+            // Reply to message id is applied by default
+            $data['reply_to_message_id'] = $message->getMessageId();
+            // Force reply is applied by default to work with privacy on
+            $data['reply_markup'] = Keyboard::forceReply(['selective' => true]);
+        }
+
+        // Start conversation
+        $message_type = $message->getType();
+
+        if (in_array($message_type, ['audio', 'document', 'photo', 'video', 'voice'], true)) {
+            $doc = $message->{'get' . ucfirst($message_type)}();
+
+            // For photos, get the best quality!
+            ($message_type === 'photo') && $doc = end($doc);
+
+            $file_id = $doc->getFileId();
+            $file    = Request::getFile(['file_id' => $file_id]);
+            if ($file->isOk() && Request::downloadFile($file->getResult())) {
+                $data['text'] = $message_type . ' file is located at: ' . $download_path . '/' . $file->getResult()->getFilePath();
+            } else {
+                $data['text'] = 'Failed to download.';
+            }
+
+        } else {
+            $data['text'] = 'Please upload the file now';
+        }
+
+        return Request::sendMessage($data);
+    }
+}
