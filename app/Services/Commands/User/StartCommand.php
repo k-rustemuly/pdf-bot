5c648bdf1d7577de9ffb6d17655268bd73bdd732
Kuanysh <developerandroidkz@gmail.com>
2024-02-13 18:41:39 +0500

    debug

---
+++ app/Services/Commands/User/StartCommand.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Services\Commands\User;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Entities\Keyboard;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Request;
+use Throwable;
+
+/**
+ * Start command
+ */
+class StartCommand extends UserCommand
+{
+
+    /**
+     * @var string
+     */
+    protected $name = 'start';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Start command';
+
+    /**
+     * @var string
+     */
+    protected $usage = '/start';
+
+    /**
+     * @var string
+     */
+    protected $version = '1.2.0';
+
+    /**
+     * @return ServerResponse
+     * @throws TelegramException
+     * @throws Throwable
+     */
+    public function execute(): ServerResponse
+    {
+
+        $message = $this->getMessage();
+        $chat    = $message->getChat();
+        $text    = trim($message->getText(true));
+        $chat_id = $chat->getId();
+        if(!empty($text))
+        {
+            return Request::sendMessage([
+                'chat_id'       => $chat_id,
+                'text'          => $text,
+            ]);
+        }
+        return Request::sendMessage([
+            'chat_id'       => $this->getMessage()->getChat()->getId(),
+            'text'          => 'aaa',
+        ]);
+        // return $this->send('Сегодня для тебя 🤌 :', $this->getMessage()->getChat()->getId(), StartKeyboard::make()->getKeyboard());
+    }
+
+    /**
+     * @throws TelegramException
+     */
+    private function send(string $text, int $chatId, Keyboard $keyboard): ServerResponse
+    {
+        return Request::sendMessage([
+            'chat_id'       => $chatId,
+            'text'          => $text,
+            'reply_markup'  => $keyboard
+        ]);
+    }
+}
