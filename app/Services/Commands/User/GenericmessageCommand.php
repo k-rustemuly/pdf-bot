8e806fd33bd79e39ef7b4f7ff9f051b489487b25
Kuanysh <developerandroidkz@gmail.com>
2024-02-13 20:20:03 +0500

    p3

---
+++ app/Services/Commands/User/GenericmessageCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * This file is part of the PHP Telegram Bot example-bot package.
+ * https://github.com/php-telegram-bot/example-bot/
+ *
+ * (c) PHP Telegram Bot Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Generic message command
+ *
+ * Gets executed when any type of message is sent.
+ *
+ * In this conversation-related context, we must ensure that active conversations get executed correctly.
+ */
+
+namespace App\Services\TelegramBots\InfoBot\Commands\System;
+
+use Longman\TelegramBot\Commands\SystemCommand;
+use Longman\TelegramBot\Conversation;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Request;
+
+class GenericmessageCommand extends SystemCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'genericmessage';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Handle generic message';
+
+    /**
+     * @var string
+     */
+    protected $version = '1.0.0';
+
+    /**
+     * @var bool
+     */
+    protected $need_mysql = true;
+
+    /**
+     * Command execute method if MySQL is required but not available
+     *
+     * @return ServerResponse
+     */
+    public function executeNoDb(): ServerResponse
+    {
+        // Do nothing
+        return Request::emptyResponse();
+    }
+
+    /**
+     * Main command execution
+     *
+     * @return ServerResponse
+     * @throws TelegramException
+     */
+    public function execute(): ServerResponse
+    {
+        $message = $this->getMessage();
+        $chat    = $message->getChat();
+        $chat_id = $chat->getId();
+
+        $data = [
+            'chat_id' => $chat_id,
+            'text'    => 'xmmmm'
+        ];
+        if($message->getType() == 'document') {
+            $doc = call_user_func('get' . $message->getType(), $message);
+            ($message->getType() === 'document') && $doc = $doc[0];
+            $file_id = $doc->getFileId();
+            $file = Request::getFile(['file_id' => $file_id]);
+            if ($file->isOk() && Request::downloadFile($file->getResult())) {
+                $data['text'] = $message->getType() . ' file is located at: ' . $this->telegram->getDownloadPath() . '/' . $file->getResult()->getFilePath();
+            }
+        }
+        return Request::sendMessage($data);
+
+        // If a conversation is busy, execute the conversation command after handling the message.
+        $conversation = new Conversation(
+            $message->getFrom()->getId(),
+            $message->getChat()->getId()
+        );
+
+        // Fetch conversation command if it exists and execute it.
+        if ($conversation->exists() && $command = $conversation->getCommand()) {
+            return $this->telegram->executeCommand($command);
+        }
+
+        return Request::emptyResponse();
+    }
+}
