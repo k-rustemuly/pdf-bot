1039b0310d40f59b866b97b2b1b4b3d0bde01696
Kuanysh <developerandroidkz@gmail.com>
2024-02-13 19:20:40 +0500

    test

--- app/Services/Commands/User/StartCommand.php
+++ app/Services/Commands/User/StartCommand.php
@@ -46,30 +46,7 @@ class StartCommand extends UserCommand
 
         $message = $this->getMessage();
         $chat    = $message->getChat();
-        // $text    = trim($message->getText(true));
         $chat_id = $chat->getId();
-        if($message->getType() == 'document') {
-            $data = [
-                'chat_id'      => $chat_id,
-            ];
-            $doc = call_user_func('get' . $message->getType(), $message);
-            ($message->getType() === 'document') && $doc = $doc[0];
-            $file_id = $doc->getFileId();
-            $file = Request::getFile(['file_id' => $file_id]);
-            if ($file->isOk() && Request::downloadFile($file->getResult())) {
-                $data['text'] = $message->getType() . ' file is located at: ' . $this->telegram->getDownloadPath() . '/' . $file->getResult()->getFilePath();
-            } else {
-                $data['text'] = 'Failed to download.';
-            }
-            return Request::sendMessage($data);
-        }
-        // if(!empty($text))
-        // {
-        //     return Request::sendMessage([
-        //         'chat_id'       => $chat_id,
-        //         'text'          => $text,
-        //     ]);
-        // }
 
         return $this->send('Сәлеметсіз бе! Бұл бот Kaspi Bank немесе Halyk Bank шотыңызда 3 ай қатарынан 100 аударым болған, болмағанын анықтауға арналған. Жұмысты бастау үшін қай банктегі шотыңызды тексергіңіз келетінін таңдаңыз.', $chat_id, StartKeyboard::make()->getKeyboard());
     }
