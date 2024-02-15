10f2ae51ee0917d6a4d678212dee37b01fea29b2
Kuanysh <developerandroidkz@gmail.com>
2024-02-15 19:16:53 +0500

    localization

--- app/Services/Keyboards/Kaspi.php
+++ app/Services/Keyboards/Kaspi.php
@@ -21,23 +21,6 @@ class Kaspi extends TelegramButton
     {
         $message = $query->getMessage();
         $accountInfo = $message->getChat();
-        $chat    = $message->getChat();
-        $chat_id = $chat->getId();
-        $data = [
-            'chat_id' => $chat_id,
-            'text'    => 'xmmmm'
-        ];
-        if($message->getType() == 'document') {
-            $doc = call_user_func('get' . $message->getType(), $message);
-            ($message->getType() === 'document') && $doc = $doc[0];
-            $file_id = $doc->getFileId();
-            $file = Request::getFile(['file_id' => $file_id]);
-            if ($file->isOk() && Request::downloadFile($file->getResult())) {
-                $data['text'] = $message->getType() . ' file is located at: ' . $this->telegram->getDownloadPath() . '/' . $file->getResult()->getFilePath();
-            }
-            return Request::sendMessage($data);
-
-        }
         return Request::sendMessage([
             'chat_id' => $accountInfo->getId(),
             'text'    => 'PDF-ті жүктеңіз'
